c0053f9d19848bd9580a790d61d75f5c7f1c183a
davips <dpsabc@gmail.com>
2021-11-29 16:00:15 -0300

    Move decolorize() to ldict

--- examples/mlworkflow.py
+++ examples/mlworkflow.py
@@ -1,12 +1,18 @@
 # Machine Learning workflow
 from pprint import pprint
 
-from idict import idict, let
+from idict import let, idict
 from idict.function.classification import fit, predict
 from idict.function.evaluation import split
 from sklearn.ensemble import RandomForestClassifier as RF
 
-d = idict.fromtoy() >> split >> let(fit, algorithm=RF, Xin="Xtr", yin="ytr") >> let(predict, Xin="Xts")
+d = (
+        idict.fromtoy()
+        >> split
+        >> let(fit, algorithm=RF, config={"n_estimators": 55}, Xin="Xtr", yin="ytr")
+        >> let(predict, Xin="Xts")
+)
+
 print(d.z)
 # ...
 
