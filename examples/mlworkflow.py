757a27561c80494ab126f7e8739418c137ef9614
davips <dpsabc@gmail.com>
2021-11-30 06:48:20 -0300

    Improve ml example

--- examples/mlworkflow.py
+++ examples/mlworkflow.py
@@ -1,20 +1,53 @@
 # Machine Learning workflow
-from pprint import pprint
+
+from sklearn.ensemble import RandomForestClassifier as RF
 
 from idict import let, idict
 from idict.function.classification import fit, predict
 from idict.function.evaluation import split
-from sklearn.ensemble import RandomForestClassifier as RF
 
+cache = {}
 d = (
         idict.fromtoy()
         >> split
         >> let(fit, algorithm=RF, config={"n_estimators": 55}, Xin="Xtr", yin="ytr")
         >> let(predict, Xin="Xts")
+        >> (lambda X: {"X2": X * X})
+        >> [cache]
 )
+print(d.y)
+# ...
+
+d.show()
+# ...
 
 print(d.z)
 # ...
 
-pprint(d.history)
+d.show()
+# ...
+
+# A field '_' means this function is a noop process triggered only once by accessing one of the other provided fields."
+d >>= (lambda _, X2, y: print("Some logging/printing that doesn't affect data... X²=\n", X2[:3]))
+d.show()
+# ...
+
+print("Triggering noop function by accessing 'y'...")
+print("y", d.y[:3])
+# ...
+
+d.show()
+# ...
+
+# The same workflow will not be processed again if the same cache is used.
+d = (
+        idict.fromtoy()
+        >> split
+        >> let(fit, algorithm=RF, config={"n_estimators": 55}, Xin="Xtr", yin="ytr")
+        >> let(predict, Xin="Xts")
+        >> (lambda X: {"X2": X * X})
+        >> (lambda _, X2, y: print("Some logging/printing that doesn't affect data...", X2.head()))
+        >> [cache]
+)
+print(d.z)
 # ...
