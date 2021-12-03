9ea75cb05e806734689449528b621eb605043072
davips <dpsabc@gmail.com>
2021-12-02 22:47:54 -0300

    Fix history step id for f with no metaid

--- examples/mlworkflow.py
+++ examples/mlworkflow.py
@@ -5,49 +5,76 @@ from sklearn.ensemble import RandomForestClassifier as RF
 from idict import let, idict
 from idict.function.classification import fit, predict
 from idict.function.evaluation import split
+from oka import Oka, generate_token
 
-cache = {}
+# Login.
+token = generate_token("http://localhost:5000")
+cache = Oka(token, "http://localhost:5000", debug=not True)
+# d = (
+#         idict.fromtoy()
+#         >> split
+#         >> let(fit, algorithm=RF, config={"n_estimators": 55}, Xin="Xtr", yin="ytr")
+#         >> let(predict, Xin="Xts")
+#         >> (lambda X: {"X2": X * X, "_history": ...})
+#     # >> [cache]
+# )
+# cache.send(d)
+# print(json.dumps(list(d.history.keys()), indent=2))
+# # ...
+#
+# d.show()
+# # ...
+#
+# print(d.z)
+# # ...
+#
+# d.show()
+# # ...
+#
+# # A field '_' means this function is a noop process triggered only once by accessing one of the other provided fields."
+# d >>= (lambda _, X2, y: print("Some logging/printing that doesn't affect data...\nX²=\n", X2[:3]))
+# d.show()
+# # ...
+#
+# print("Triggering noop function by accessing 'y'...")
+# print("y", d.y[:3])
+# # ...
+#
+# d.show()
+# # ...
+#
+# # The same workflow will not be processed again if the same cache is used.
 d = (
         idict.fromtoy()
         >> split
         >> let(fit, algorithm=RF, config={"n_estimators": 55}, Xin="Xtr", yin="ytr")
         >> let(predict, Xin="Xts")
-        >> (lambda X: {"X2": X * X})
-        >> [cache]
+        # >> (lambda X: {"X2": X * X})
+        # >> (lambda _, X2, y: print("Some logging/printing that doesn't affect data...", X2.head()))
+    # >> [cache]
 )
-print(d.y)
-# ...
+# print("oid", d.id)
+# d.show()
+# print("--------------------------")
+# print()
+# print()
 
+cache.send(d)
+
+print("fields", d.id)
+d = cache.get(d.id)
 d.show()
+print("--------------------------")
+print()
+print()
 # ...
 
-print(d.z)
-# ...
+exit()
 
+print("metafields", d.hosh * cache.uhosh)
+d = cache.get(d.hosh * cache.uhosh)
 d.show()
-# ...
-
-# A field '_' means this function is a noop process triggered only once by accessing one of the other provided fields."
-d >>= (lambda _, X2, y: print("Some logging/printing that doesn't affect data... X²=\n", X2[:3]))
-d.show()
-# ...
-
-print("Triggering noop function by accessing 'y'...")
-print("y", d.y[:3])
-# ...
-
-d.show()
-# ...
-
-# The same workflow will not be processed again if the same cache is used.
-d = (
-        idict.fromtoy()
-        >> split
-        >> let(fit, algorithm=RF, config={"n_estimators": 55}, Xin="Xtr", yin="ytr")
-        >> let(predict, Xin="Xts")
-        >> (lambda X: {"X2": X * X})
-        >> (lambda _, X2, y: print("Some logging/printing that doesn't affect data...", X2.head()))
-        >> [cache]
-)
-print(d.z)
+print("--------------------------")
+print()
+print()
 # ...
