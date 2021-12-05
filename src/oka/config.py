3aabc7fe3aec1a7339943a7b45de914fe2dcb500
davips <dpsabc@gmail.com>
2021-12-05 00:15:08 -0300

    Fix gh tests

--- src/oka/config.py
+++ src/oka/config.py
@@ -2,7 +2,7 @@ import os
 
 if "OKATESTING" in os.environ:
     # user:pass@http://localhost:5000
-    default_url = os.environ['OKATESTING'].split("@")[1]
+    default_url = os.environ["OKATESTING"].split("@")[1]
     default_user, default_password = os.environ["OKATESTING"].split("@")[0].split(":")
     print("OKATESTING var in use", default_user, default_url)
 else:
